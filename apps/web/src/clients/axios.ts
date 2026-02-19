23f4a3890d80476ffeb938be25d0524cf091be93
Christian Llansola <christian.llansola@mail.schwarz>
2026-02-19 10:10:58 +0100

    endpoint axios client

--- apps/web/src/clients/axios.ts
+++ apps/web/src/clients/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:3001',
+  baseURL: 'https://payment-api-tawny.vercel.app',
   headers: {
     'Content-Type': 'application/json'
   }
