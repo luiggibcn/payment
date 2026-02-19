5b23a8af42c8d74b68cfb8bcc4c8ed117f91f9d2
Christian Llansola <christian.llansola@mail.schwarz>
2026-02-19 10:23:05 +0100

    api url env

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -110,7 +110,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: process.env.VITE_API_URL,
         changeOrigin: true
       }
     }
