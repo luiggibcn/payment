134313917afc6277bf850be31bb155d1efd2d2e4
Christian Llansola <christian.llansola@gmail.com>
2026-02-22 13:30:01 +0100

    CORS API

--- apps/api/nuxt.config.ts
+++ apps/api/nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
     '/api/**': {
       cors: true,
       headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:5173',
+        'Access-Control-Allow-Origin': process.env.WEB_URL ?? 'http://localhost:5173',
         'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         // 'Access-Control-Allow-Headers': 'X-PAYMENT, X-DATA',  // // Headers custom del BE, no se activaran hasta que negocio decida que es necesario
